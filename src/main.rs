0dd7a1cf4bc5fad9103e84d38d1d792615d8c9f2
megahomyak <g.megahomyak@gmail.com>
2023-06-15 15:51:11 +0500

    Was writing the actions registry, decided to go simpler for now

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,47 @@
-use clap::{Parser, Subcommand};
+use std::collections::HashMap;
 
-#[derive(Subcommand)]
-enum Action {
-    Run { script_file_path: String },
+use clap::Parser;
+
+mod actions_registry;
+
+mod cmd {
+    use clap::{Parser, Subcommand};
+
+    #[derive(Subcommand)]
+    pub enum Action {
+        Run { script_file_path: String },
+    }
+
+    #[derive(Parser)]
+    #[command(author, version, about)]
+    pub struct Args {
+        #[command(subcommand)]
+        action: Action,
+    }
 }
 
-#[derive(Parser)]
-#[command(author, version, about)]
-struct CmdArgs {
-    #[command(subcommand)]
-    action: Action,
+
+#[derive(PartialEq, Eq, Hash)]
+struct Name {
+    contents: Vec<NamePart>,
+}
+
+struct Action {}
+
+struct Executor {
+    execute: HashMap<Name, Action>,
+}
+
+impl Executor {
+    fn run(&self, program: hzlang_parser::Program) -> Result<(), ()> {
+        for action_invocation in program.contents {
+
+        }
+    }
 }
 
 fn main() {
-    let cmd_args = CmdArgs::parse();
+    let cmd_args = cmd::Args::parse();
     match cmd_args.action {
         Action::Run { script_file_path } => println!("{}", script_file_path),
     }
